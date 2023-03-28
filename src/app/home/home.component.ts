a3aa3cb1b20879801001a80d68a941e851a2d97c
Noir <vokhuong1403@gmail.com>
2023-03-29 00:10:39 +0700

    Xong bai 22

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { product } from './../data-type';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  popularProducts: undefined | product[];
+
+  constructor(private product: ProductService) {
+    
+  }
+
+  ngOnInit(): void {
+      this.product.popularProducts().subscribe((data) => {
+        this.popularProducts = data;
+      })
+  }
 
 }
